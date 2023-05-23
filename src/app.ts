26466310cbf57d697a5716695e3bddb3bb6ef967
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-05-23 18:05:09 +0300

    feat: install pg and dotenv, create a test DB connection

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
+import * as dotenv from 'dotenv';
 import express, { Request, Response, Application } from 'express';
+import dbQuery from './db';
+
+dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get('/', async (req: Request, res: Response) => {
+  const dbRes = await dbQuery('SELECT $1::text as message', ['Hello DB!']);
+  res.send(dbRes.rows[0].message);
 });
 
 app.listen(port, () => {
