185ce576929943755e94c85b53f5edf800cc7c4c
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-05-25 13:00:10 +0300

    feat: add query for creating users table in db

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import * as dotenv from 'dotenv';
 import express from 'express';
-import dbQuery from './db';
+import { dbQuery, createTables } from './db';
 import { usersRouter, loginRouter, boardsRouter, columnsRouter, tasksRouter, fileRouter } from './routers';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+createTables();
 
 app.get('/', async (req, res) => {
   const dbRes = await dbQuery('SELECT $1::text as message', ['Hello DB!']);
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,21 @@ import { Pool } from 'pg';
 
 const pool = new Pool({ ssl: true });
 
-const dbQuery = (text: string, params: string[]) => pool.query(text, params);
+const dbQuery = (text: string, params?: string[]) => pool.query(text, params);
 
-export default dbQuery;
+const createTables = async () => {
+  const res = await dbQuery(
+    `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+     CREATE TABLE IF NOT EXISTS "users" (
+       "id" uuid NOT NULL DEFAULT uuid_generate_v4(), 
+       "name" character varying NOT NULL, 
+       "login" character varying NOT NULL, 
+       "password" character varying NOT NULL, 
+       CONSTRAINT "UQ_2d443082eccd5198f95f2a36e2c" UNIQUE ("login"),
+       CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id")
+     )`,
+  );
+  console.log(res);
+};
+
+export { dbQuery, createTables };
