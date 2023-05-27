23296224d358f2e0d36aee0969c19134f3faca20
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-05-27 21:09:46 +0300

    feat: add query for 'boards' table, make id columns always generated

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,11 +8,16 @@ const createTables = async () => {
   const res = await dbQuery(
     `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
      CREATE TABLE IF NOT EXISTS "users" (
-       "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(), 
+       "id" uuid PRIMARY KEY GENERATED ALWAYS AS (uuid_generate_v4()), 
        "name" VARCHAR (25) NOT NULL, 
        "login" VARCHAR (25) UNIQUE NOT NULL, 
        "password" VARCHAR (25) NOT NULL
-     )`,
+     );
+     CREATE TABLE IF NOT EXISTS "boards" (
+       "id" uuid PRIMARY KEY GENERATED ALWAYS AS (uuid_generate_v4()),
+       "title" character varying NOT NULL,
+       "description" character varying NOT NULL
+     );`,
   );
   console.log(res);
 };
