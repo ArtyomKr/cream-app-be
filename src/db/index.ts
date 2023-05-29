4aa8bc8b5a962e7967d0f26b93fbcbf1c05c72d2
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-05-29 17:52:05 +0300

    feat: add queries for 'columns', 'tasks' and 'files' tables

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,15 +8,46 @@ const createTables = async () => {
   const res = await dbQuery(
     `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
      CREATE TABLE IF NOT EXISTS "users" (
-       "id" uuid PRIMARY KEY GENERATED ALWAYS AS (uuid_generate_v4()), 
+       "id" uuid DEFAULT uuid_generate_v4(), 
        "name" VARCHAR (25) NOT NULL, 
        "login" VARCHAR (25) UNIQUE NOT NULL, 
-       "password" VARCHAR (25) NOT NULL
+       "password" VARCHAR (25) NOT NULL,
+       PRIMARY KEY ("id")
      );
      CREATE TABLE IF NOT EXISTS "boards" (
-       "id" uuid PRIMARY KEY GENERATED ALWAYS AS (uuid_generate_v4()),
-       "title" character varying NOT NULL,
-       "description" character varying NOT NULL
+       "id" uuid DEFAULT uuid_generate_v4(),
+       "title" VARCHAR NOT NULL,
+       "description" VARCHAR NOT NULL,
+       PRIMARY KEY ("id")
+     );
+     CREATE TABLE IF NOT EXISTS "columns" (
+       "id" uuid DEFAULT uuid_generate_v4(),
+       "title" VARCHAR NOT NULL,
+       "order" SMALLINT NOT NULL,
+       "boardId" uuid REFERENCES "boards" ("id")
+         ON DELETE CASCADE ON UPDATE CASCADE,
+       PRIMARY KEY ("id")
+     );
+     CREATE TABLE IF NOT EXISTS "tasks" (
+       "id" uuid DEFAULT uuid_generate_v4(),
+       "title" VARCHAR NOT NULL,
+       "order" SMALLINT NOT NULL,
+       "description" VARCHAR NOT NULL,
+       "userId" uuid REFERENCES "users" ("id")
+         ON DELETE SET NULL ON UPDATE CASCADE,
+       "boardId" uuid REFERENCES "boards" ("id")
+         ON DELETE CASCADE ON UPDATE CASCADE,
+       "columnId" uuid REFERENCES "columns" ("id")
+         ON DELETE CASCADE ON UPDATE CASCADE,
+       PRIMARY KEY ("id")
+     );
+     CREATE TABLE IF NOT EXISTS "files" (
+       "filename" VARCHAR UNIQUE NOT NULL,
+       "fileId" VARCHAR NOT NULL,
+       "fileSize" INTEGER NOT NULL,
+       "taskId" uuid REFERENCES "tasks" ("id")
+         ON DELETE CASCADE ON UPDATE CASCADE,
+       PRIMARY KEY ("fileId")
      );`,
   );
   console.log(res);
