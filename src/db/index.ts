b8415cda36efac29a12663911546c553b9a27eb5
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-01 21:24:49 +0300

    feat: add findUser function for searching user in DB

--- src/db/index.ts
+++ src/db/index.ts
@@ -55,11 +55,22 @@ const createUser = async (name: string, login: string, password: string) => {
   const res = await dbQuery(
     `INSERT INTO users (name, login, password)
         VALUES ($1, $2, $3)
-        RETURNING id, name, login`,
+        RETURNING id, name, login;`,
     [name, login, password],
   );
 
   return res.rows[0];
 };
 
-export { dbQuery, createTables, createUser };
+const findUser = async (login: string) => {
+  const res = await dbQuery(
+    `SELECT id, password
+        FROM users
+        WHERE login = $1;`,
+    [login],
+  );
+
+  return res.rows[0];
+};
+
+export { dbQuery, createTables, createUser, findUser };
