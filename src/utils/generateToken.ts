5899e4ec1cb369af12f310dc033fc56b4b5cece4
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-03 22:12:10 +0300

    refactor: change payload signed by jwt

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,7 +1,8 @@
 import { sign } from 'jsonwebtoken';
 
 function generateToken(id: string, login: string) {
-  return sign({ userId: id, login }, process.env.JWT_SECRET_KEY as string);
+  // TODO: add expire time
+  return sign({ userId: id, userLogin: login }, process.env.JWT_SECRET_KEY as string);
 }
 
 export default generateToken;
