8876671513793cc122a9f0e5c9f2c1f0ea5eebad
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-11 18:17:14 +0300

    feat: add typeguard for edit task request

--- src/routers/tasks/typeGuards.ts
+++ src/routers/tasks/typeGuards.ts
@@ -1,14 +1,26 @@
-import { ITaskRequest } from '../../models/apiModels';
+import { ITask, ITaskRequest } from '../../models/apiModels';
 
 const isTaskRequestBody = (body: object): body is ITaskRequest => {
+  return (
+    'title' in body && 'description' in body && typeof body.title === 'string' && typeof body.description === 'string'
+  );
+};
+
+const isTaskEditRequestBody = (body: object): body is Omit<ITask, 'id'> => {
   return (
     'title' in body &&
     'order' in body &&
     'description' in body &&
+    'userId' in body &&
+    'columnId' in body &&
+    'boardId' in body &&
     typeof body.title === 'string' &&
     typeof body.order === 'number' &&
-    typeof body.description === 'string'
+    typeof body.description === 'string' &&
+    typeof body.userId === 'string' &&
+    typeof body.columnId === 'string' &&
+    typeof body.boardId === 'string'
   );
 };
 
-export default isTaskRequestBody;
+export { isTaskRequestBody, isTaskEditRequestBody };
