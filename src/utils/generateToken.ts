b310c45776a169031720a29d30e4e92020215606
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-12 21:25:54 +0300

    refactor: remove TODO

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,7 +1,6 @@
 import { sign } from 'jsonwebtoken';
 
 function generateToken(id: string, login: string) {
-  // TODO: add expire time
   return sign({ userId: id, userLogin: login }, process.env.JWT_SECRET_KEY as string);
 }
 
