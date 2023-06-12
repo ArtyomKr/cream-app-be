a01648841ecb38e182300c5b997b86751692e56b
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-12 21:29:21 +0300

    refactor: add pre flight db queries, remove /files router

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,17 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import { dbQuery, createTables } from './db';
-import { usersRouter, authRouter, boardsRouter, columnsRouter, tasksRouter, fileRouter } from './routers';
+import { usersRouter, authRouter, boardsRouter, columnsRouter, tasksRouter } from './routers';
+import { createColumnsTrigger, createTables, createTasksTrigger } from './db/preFlight';
 
 dotenv.config();
 
+createTables()
+  .then(() => createColumnsTrigger())
+  .then(() => createTasksTrigger());
+
 const app = express();
 const port = process.env.PORT || 3000;
-
-createTables();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -20,13 +22,7 @@ app.use(authRouter);
 app.use(boardsRouter);
 app.use(columnsRouter);
 app.use(tasksRouter);
-app.use(fileRouter);
-
-app.get('/', async (req, res) => {
-  const dbRes = await dbQuery('SELECT $1::text as message', ['Hello DB!']);
-  res.send(dbRes.rows[0].message);
-});
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+  return console.log(`Express is listening at the following port: ${port}`);
 });
