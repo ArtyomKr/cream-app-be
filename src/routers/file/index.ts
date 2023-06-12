d5f54da8f9a343ba66fb99143434b3e51f17e5de
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-06-12 21:13:37 +0300

    refactor: delete /files route

--- src/routers/file/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const fileRouter = express.Router();
-
-fileRouter.get('/file', (req, res) => {
-  res.send('File funtionality will be here');
-});
-
-export default fileRouter;
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,4 +3,3 @@ export { default as authRouter } from './auth';
 export { default as boardsRouter } from './boards';
 export { default as columnsRouter } from './columns';
 export { default as tasksRouter } from './tasks';
-export { default as fileRouter } from './file';
